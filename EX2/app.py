f34602828c1050d9b87ceac47ebaa0ccc3ce043d
Joel R Tharkan <joelrtharakan@gmail.com>
2026-04-22 23:23:07 +0530

    Initial Commit

--- EX2/app.py
+++
@@ -1,3 +0,0 @@
-# app.py
-# This file prints a simple message to show the CI lab is working.
-print("CI Lab Working")
--- EX2/test_app.py
+++
@@ -1,4 +0,0 @@
-# test_app.py
-# This test checks that the CI setup can run a simple test.
-def test_ci():
-    assert True
